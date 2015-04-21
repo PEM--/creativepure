3c2d35c2eef57f13ebeae5b74e3be8caa8acccf4
PEM-- <pemarchandet@gmail.com>
2015-04-21 20:09:45 +0200

    Long shadow

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pierreeric:creativepure',
-  version: '0.1.11',
+  version: '0.1.12',
   // Brief, one-line summary of the package.
   summary: 'A set of styles all written with Stylus and inspired from Codrops.',
   // URL to the Git repository containing the source code for this package.
